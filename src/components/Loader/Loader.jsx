4349ef755aa81822c768379042356af6b6839ced
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-08-09 12:11:03 +0200

    Changes color of Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -9,7 +9,7 @@ const Loader = () => {
           height="80"
           width="80"
           radius="9"
-          color="blue"
+          color="black"
           ariaLabel="three-dots-loading"
           visible={true}
         />
